0c57c3f888598c82df20bbbd589272a3251cd36c
filimon4uck <filimon4uck.yura@gmail.com>
2024-08-06 17:22:35 +0200

    ranamed parameters and renamed name of file object

--- address_book/serialize.py
+++ address_book/serialize.py
@@ -2,14 +2,14 @@ import pickle
 from address_book import AddressBook
 
 
-def save_data(book, filename="addressbook.pkl"):
-    with open(filename, "wb") as f:
-        pickle.dump(book, f)
+def save_data(book, filename="addressbook_data.pkl"):
+    with open(filename, "wb") as file:
+        pickle.dump(book, file)
 
 
-def load_data(filename="addressbook.pkl"):
+def load_data(filename="addressbook_data.pkl"):
     try:
-        with open(filename, "rb") as f:
-            return pickle.load(f)
+        with open(filename, "rb") as file:
+            return pickle.load(file)
     except FileNotFoundError:
         return AddressBook()
